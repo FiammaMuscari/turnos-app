279769b4b686372fae3a835d6d377289f03c6081
fiammamuscari <fiammamuscari@gmail.com>
2024-01-17 18:15:18 -0300

    fix: redirect on login

--- routes.ts
+++ routes.ts
@@ -3,14 +3,11 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = [
-  "/",
-  "/auth/new-verification"
-];
+export const publicRoutes = ["/", "/auth/new-verification"];
 
 /**
  * An array of routes that are used for authentication
- * These routes will redirect logged in users to /settings
+ * These routes will redirect logged in users to /turno
  * @type {string[]}
  */
 export const authRoutes = [
@@ -18,7 +15,7 @@ export const authRoutes = [
   "/auth/register",
   "/auth/error",
   "/auth/reset",
-  "/auth/new-password"
+  "/auth/new-password",
 ];
 
 /**
@@ -32,4 +29,4 @@ export const apiAuthPrefix = "/api/auth";
  * The default redirect path after logging in
  * @type {string}
  */
-export const DEFAULT_LOGIN_REDIRECT = "/settings";
+export const DEFAULT_LOGIN_REDIRECT = "/turno";
