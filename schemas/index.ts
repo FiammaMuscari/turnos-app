6a5fd1d8075b1b803cd3510d41e01799e85b89c1
fiammamuscari <fiammamuscari@gmail.com>
2024-01-18 19:01:31 -0300

    fix: deploy

--- schemas/index.ts
+++ schemas/index.ts
@@ -43,6 +43,33 @@ export const NewPasswordSchema = z.object({
   }),
 });
 
+export const ResetSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+});
+
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+  code: z.optional(z.string()),
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(6, {
+    message: "Minimum 6 characters required",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
 export const ServiceSchema = z.object({
   name: z.string().min(4, {
     message: "Minimum 4 characters required",
