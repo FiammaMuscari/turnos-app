7726b8f860a98a94c47e4bac28917d3472960778
Fiammamuscari <fiammamuscari@gmail.com>
2024-04-25 11:29:35 -0300

    fix:deploy

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,27 +1,16 @@
 import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+
 import { MercadoPagoConfig, Payment } from "mercadopago";
 
 const mercadopago = new MercadoPagoConfig({
   accessToken: process.env.MP_ACCESS_TOKEN!,
 });
 
-export default async function GET(req: NextRequest, res: NextResponse) {
-  const searchParams = new URLSearchParams(req.url?.split("?")[1] || "");
-  const topic = searchParams.get("topic") || searchParams.get("type");
-
-  console.log({ topic });
-
-  try {
-    if (topic === "payment") {
-      const body = req.body as unknown as { data: { id: string } };
-      const payment = await new Payment(mercadopago).get({ id: body.data.id });
-      return new NextResponse(null, { status: 200 });
-    } else {
-      return new NextResponse(null, { status: 400 });
-    }
-  } catch (error) {
-    console.error("Error processing payment:", error);
-    return new NextResponse(null, { status: 500 });
-  }
+export async function POST(request: NextRequest) {
+  const body = await request
+    .json()
+    .then((data) => data as { data: { id: string } });
+  const payment = await new Payment(mercadopago).get({ id: body.data.id });
+  console.log({ payment });
+  return Response.json({ success: true });
 }
