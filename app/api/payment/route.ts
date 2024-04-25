b4eac195fa5930c833df44d5918aecf22fec8646
Fiammamuscari <fiammamuscari@gmail.com>
2024-04-25 11:05:04 -0300

    fix: webhook api route

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -9,16 +9,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).end();
-  }
+  const searchParams = new URLSearchParams(req.url?.split("?")[1] || "");
+  const topic = searchParams.get("topic") || searchParams.get("type");
+
+  console.log({ topic });
 
   try {
-    const body = req.body as { data: { id: string } };
-
-    const payment = await new Payment(mercadopago).get({ id: body.data.id });
-
-    return res.status(200).json({ success: true });
+    if (topic === "payment") {
+      const body = req.body as { data: { id: string } };
+      const payment = await new Payment(mercadopago).get({ id: body.data.id });
+      return res.status(200).json({ payment });
+    } else {
+      return res.status(400).json({ message: "Invalid topic" });
+    }
   } catch (error) {
     console.error("Error processing payment:", error);
     return res.status(500).json({ error: "Error processing payment" });
