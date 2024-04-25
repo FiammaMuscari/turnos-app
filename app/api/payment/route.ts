e7e589f5effcf174ef26c149fc9cc8fc069725be
Fiammamuscari <fiammamuscari@gmail.com>
2024-04-25 10:56:16 -0300

    fix: webhook notification

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,17 +1,26 @@
-import type { NextRequest } from "next/server";
-
+import { NextApiRequest, NextApiResponse } from "next";
 import { MercadoPagoConfig, Payment } from "mercadopago";
 
 const mercadopago = new MercadoPagoConfig({
   accessToken: process.env.MP_ACCESS_TOKEN!,
 });
 
-export async function POST(request: NextRequest) {
-  const body = await request
-    .json()
-    .then((data) => data as { data: { id: string } });
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
 
-  const payment = await new Payment(mercadopago).get({ id: body.data.id });
+  try {
+    const body = req.body as { data: { id: string } };
 
-  return Response.json({ success: true });
+    const payment = await new Payment(mercadopago).get({ id: body.data.id });
+
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("Error processing payment:", error);
+    return res.status(500).json({ error: "Error processing payment" });
+  }
 }
