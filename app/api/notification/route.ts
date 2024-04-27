22671c68aab241eecdb9fa55519b2def80f8bcc3
Fiammamuscari <fiammamuscari@gmail.com>
2024-04-27 18:03:49 -0300

    feat: route notification

--- app/api/notification/route.ts
+++ app/api/notification/route.ts
@@ -1,5 +1,50 @@
 import { NextResponse } from "next/server";
 
-export async function POST() {
-  return new NextResponse(null, { status: 200 });
+async function handler(request: Request) {
+  const payload = (await request.json()) as {
+    action: string;
+    type: string;
+    data: { id: string };
+  };
+  const {
+    type,
+    data: { id },
+    action,
+  } = payload;
+  console.log(id, type);
+  console.log(payload);
+  if (type === "payment" || type === "refund") {
+    try {
+      const responseCompra = await fetch(
+        `https://api.mercadopago.com/v1/payments/${id}`,
+        {
+          headers: {
+            "Content-type": "application/json",
+            Authorization: `Bearer ${process.env.MP_ACCESS_TOKEN! as string}`,
+          },
+        }
+      );
+      console.log(responseCompra);
+      if (!responseCompra.ok) {
+        throw new Error(`HTTP error! status: ${responseCompra.status}`);
+      }
+      const data = (await responseCompra.json()) as {
+        metadata: { budget_id: string; description: string };
+        status: string;
+        status_detail: string;
+      };
+      console.log(data);
+      console.log(data.metadata);
+      if (!data.metadata) {
+        throw new Error(`HTTP error! status: ${responseCompra.status}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (type === "mp-connect" && action === "application.authorized") {
+    console.log(payload);
+  }
+
+  return NextResponse.json({}, { status: 200 });
 }
+export const POST = handler;
